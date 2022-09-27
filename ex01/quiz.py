34cf6fbd07904d8bde55b60eba7ed717d72802dc
kazuto <c0b21081fa@edu.teu.ac.jp>
2022-09-27 17:09:02 +0900

    コメント

---
+++ ex01/quiz.py
@@ -0,0 +1,23 @@
+import random
+
+def shutudai(qa_lst):
+    i = random.choice(qa_lst)
+    print("問題："+i["q"])
+    return i["a"]
+
+def kaito(ans_lst):
+    ans = input("答えるんだ：")
+    if ans in ans_lst:
+        print("正解")
+    else:
+        print("不正解")
+
+if __name__ == "__main__":
+    qa_lst = {
+        {"q":"サザエの旦那は？", "a": ["ますお", "マスオ"]},
+        {"q":"カツオの妹は？", "a": ["わかめ", "ワカメ"]},
+        {"q":"タラオはカツオからみて何？", "a": ["おい", "オイ", "甥", "おいっこ"]}
+    }
+
+    ans_lst = shutudai(qa_lst)
+    kaito(ans_lst)
