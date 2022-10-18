5ebfac82782ea8ae2ce2e375d96ebaf7360edf77
kazuto <c0b21081fa@edu.teu.ac.jp>
2022-10-18 14:22:14 +0900

    練習問題3-4

---
+++ ex03/maze.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.title("迷えるこうかとん")   #練習１
+    
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack()
+
+    tori = tk.PhotoImage(file="fig/5.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori")
+
+    root.mainloop()
