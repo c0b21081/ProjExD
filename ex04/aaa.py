b1a91979f1b7d5b2a8e6ff5063bb9a755e316b14
kazuto <c0b21081fa@edu.teu.ac.jp>
2022-10-25 17:32:19 +0900

    終了

---
+++ ex04/aaa.py
@@ -0,0 +1,22 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("初めてのPyGame")
+    scrn_sfc = pg.display.set_mode((800, 600))
+
+    tori_sfc = pg.image.load("fig/6.png") # Surface
+    tori_rct = tori_sfc.get_rect() # Rect
+    tori_rct.center = 700, 400 # スクリーンにこうかとんを張り付ける
+    scrn_sfc.blit(tori_sfc, tori_rct)
+
+    pg.display.update()
+
+    clock = pg.time.Clock()
+    clock.tick(0.2)
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
